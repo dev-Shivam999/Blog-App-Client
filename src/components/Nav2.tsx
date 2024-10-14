ae365367325368ef7cc37a79e84ae2f1f25964dd
dev-Shivam999 <shivamswami982@gmail.com>
2024-10-14 22:17:42 +0530

    make logo img

--- src/components/Nav2.tsx
+++ src/components/Nav2.tsx
@@ -1,10 +1,13 @@
 
 import { Link } from 'react-router-dom';
+import Img from './Img';
 
 const Nav2 = () => {
     return (
         <>
-            <Link to={'/'}><h1 className='text-[4vw] sm:text-[2vw]'>CRO.BLOGS</h1></Link>
+            <Link to={'/'} className=' '><h1 className=' flex  items-center  gap-5 text-[4vw] sm:text-[2vw]'>
+                <Img className='rounded-full w-20' val='/FIRST.png'/>
+                CRO.BLOGS</h1></Link>
             <Link to={'/Blogs'}><h1 className='text-[4vw] sm:text-[2vw] text-end' >BLOGS</h1></Link>   
         </>
     );
