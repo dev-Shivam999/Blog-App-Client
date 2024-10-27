00f02e6549752c22db17a205658a753c41e4020b
dev-Shivam999 <shivamswami982@gmail.com>
2024-10-27 21:47:40 +0530

    chang loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,14 @@
 import "./Load.css"
 export const BookLoaderComponent = () => {
     return (
-        <div className="book">
-            <div className="book__pg-shadow"></div>
-            <div className="book__pg"></div>
-            <div className="book__pg book__pg--2"></div>
-            <div className="book__pg book__pg--3"></div>
-            <div className="book__pg book__pg--4"></div>
-            <div className="book__pg book__pg--5"></div>
+        <div id="load">
+            <div>G</div>
+            <div>N</div>
+            <div>I</div>
+            <div>D</div>
+            <div>A</div>
+            <div>O</div>
+            <div>L</div>
         </div>
     );
 };
