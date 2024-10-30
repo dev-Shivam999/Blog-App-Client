530f6d17f5bda92847f71d44c901134cabaff8c2
dev-Shivam999 <shivamswami982@gmail.com>
2024-10-30 22:52:56 +0530

    add socket.io

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,13 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import ChatNav from "../components/ChatNav";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
 
 
 const Chat = () => {
+
+    const socket = useMemo(() => io("http://localhost:3000"), []);
     const { id } = useParams()
+    useEffect(() => {
+        socket.on("message", (data) => {
+            console.log(data);
+
+        });
+
+
+        return () => {
+            socket.off('message');
+        };
+    }, [])
     useEffect(() => {
 
         axios.post(`${import.meta.env.VITE_SOME_KEY}/user/profile`, "none", {
@@ -19,22 +33,23 @@ const Chat = () => {
         }).then(
             res => {
                 if (res.data.success) {
-                   
 
 
-                  
+
+
                 } else {
                     if (res.data.message == 'Not found') {
                         toast.error("Blogger not found")
                     }
 
-            }}
+                }
+            }
         ).catch(err => console.log(err))
 
     }, [])
     return (
         <div>
-     <ChatNav/>
+            <ChatNav />
         </div>
     );
 };
