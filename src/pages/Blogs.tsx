61e97ba7bb41168ec94ffd37983e6d4239b615f1
dev-Shivam999 <shivamswami982@gmail.com>
2025-02-14 13:12:36 +0530

    add react-masonry-css for improved blog layout

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,4 +1,3 @@
-
 import BlogCard from '../components/BlogCard';
 import MidNav from '../components/MidNav';
 import { useSelector } from 'react-redux';
@@ -8,6 +7,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import FirstCard from '../components/FirstCard';
 import { BookLoaderComponent } from '../components/Loading';
+import Masonry from 'react-masonry-css';
 
 const Blogs = () => {
 
@@ -20,52 +20,49 @@ const Blogs = () => {
             }
         }).then(
             (res) => {
-
-
-                dispatch(setBlogs({ blogs: res.data.blogs, loading: false, val: res.data.vali,su:res.data.success }))
-
+                dispatch(setBlogs({ blogs: res.data.blogs, loading: false, val: res.data.vali, su: res.data.success }))
             }
         ).catch(
             err => {
-                dispatch(setBlogs({ blogs: [], loading: false, val: "",su:false }))
+                dispatch(setBlogs({ blogs: [], loading: false, val: "", su: false }))
                 console.log(err)
             }
-    )
+        )
 
     }, [])
-    const { loading, blogs, val,su } = useSelector((state: InitialState2) => state.b);
+    const { loading, blogs, val, su } = useSelector((state: InitialState2) => state.b);
 
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 4,
+        992: 3,
+        768: 2,
+        576: 1
+    };
 
-
-    
     return (
         <div className='p-4'>
             <div className=' '>
-             
-           
-                        <MidNav val2={su} val={val} />
-               
-
+                <MidNav val2={su} val={val} />
             </div>
             {
-                loading ? <BookLoaderComponent/> : blogs && blogs.length > 0 ? <>
-                    <h1 className="font-bold text-3xl">OUR POPULAR  BLOG</h1>
+                loading ? <BookLoaderComponent /> : blogs && blogs.length > 0 ? <>
+                    <h1 className="font-bold text-3xl">OUR POPULAR BLOG</h1>
 
                     <FirstCard Like={blogs[0]?.Likes!} authorName={blogs[0].authore.name} authorPic={blogs[0].authore.img} content={blogs[0].content} publishedDate={blogs[0].created} id={blogs[0].id} BlogerId={blogs[0].authore.id} title={blogs[0].title} avatar={blogs[0].avtar} />
-                 <div className='grid gap-2 grid-cols-1 sm:grid-cols-2'>
+                    <Masonry
+                        breakpointCols={breakpointColumnsObj}
+                        className="flex flex-wrap gap-1 my-masonry-grid"
+                        columnClassName="my-masonry-grid_column">
                         {
-                            blogs.map((p, i) => i !== 0 && <BlogCard key={p.id} Like={p?.Likes!} authorName={p.authore.name} authorPic={p.authore.img} content={p.content} publishedDate={p.created} id={p.id} BlogerId={p.authore.id} title={p.title} avatar={p.avtar} />
-                            )
+                            blogs.map((p, i) => i !== 0 && <BlogCard key={p.id} Like={p?.Likes!} authorName={p.authore.name} authorPic={p.authore.img} content={p.content} publishedDate={p.created} id={p.id} BlogerId={p.authore.id} title={p.title} avatar={p.avtar} />)
                         }
-                 </div>
-                    
-                </>:<div>
-                        {!su ?"server off try again":"Add BLogs"}
-                    
+                    </Masonry>
+                </> : <div>
+                    {!su ? "server off try again" : "Add Blogs"}
                 </div>
             }
         </div>
-
     );
 };
 
